af90b284b9348b4ed5f126e0ab66c9df158d5610
KDibacco <katrinadibacco@hotmail.com>
2024-05-21 18:36:29 -0400

    Adding new code to Lab 3 in R

--- Lab3.R
+++ Lab3.R
@@ -4,13 +4,13 @@ library(readr)
 train <- read_csv("~/Documents/PhD/MEV 6700/train.csv")
 View(train)
 
-#install.packages("usethis")
+install.packages("usethis")
 library ("usethis")
 
 use_git_config(user.name = "KDibacco", user.email = "katrinadibacco@hotmail.com")
 edit_git_config()
 use_git()
-1
+
 
 library('tidyverse')
 # Download data to R
@@ -21,6 +21,10 @@ summary(new_data) # Summarize each variable.
 
 new_data %>% ggplot(aes(x = x0)) + geom_histogram()
 
+library(usethis)
+create_github_token()
+library(gitcreds)
+gitcreds_set()
+2
 
-# let's cerate a boxplot for the same variable "x0"
-new_data %>% ggplot(aes(x = x0)) + geom_box()
+use_github()
