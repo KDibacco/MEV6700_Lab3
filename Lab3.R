830aaf6aaf2695bc3205d49996153483f4d9c3ce
KDibacco <katrinadibacco@hotmail.com>
2024-05-21 13:32:18 -0400

    Initial commit

--- Lab3.R
+++ Lab3.R
@@ -4,3 +4,8 @@ library(readr)
 train <- read_csv("~/Documents/PhD/MEV 6700/train.csv")
 View(train)
 
+#install.packages("usethis")
+library ("usethis")
+
+use_git_config(user.name = "KDibacco", user.email = "katrinadibacco@hotmail.com")
+edit_git_config()
